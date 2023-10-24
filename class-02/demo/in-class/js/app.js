2b127331f23c72ae0442d3dd5e557735557286d1
JB Tellez <jb.tellez@gmail.com>
2023-10-24 12:14:06 -0700

    adds  linked files

---
+++ class-02/demo/in-class/js/app.js
@@ -0,0 +1,24 @@
+
+let timeToQuit = false;
+let name = 'jb'; // remember in lab about toUpperCase toLowerCase
+
+// if (name === 'JB') {
+//   alert("TRUE");
+// } else if (name === 'jb') {
+//   alert('TRUE');
+// } else {
+//   alert("FALSE");
+// }
+
+switch (name) {
+  case 'JB':
+    alert('yep');
+    break;
+  case 'DeAndre':
+    alert("nope, but heard he's a great dev");
+    break;
+  default:
+    alert('never heard of them');
+}
+
+
