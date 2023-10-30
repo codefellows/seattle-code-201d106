c8e615bede952a13b4487fc13884ed2a081e93fb
JB Tellez <jb.tellez@gmail.com>
2023-10-29 23:03:54 -0700

    streamlines demo

--- class-06/demo/domain-modeling-and-dom/js/app.js
+++ class-06/demo/domain-modeling-and-dom/js/app.js
@@ -16,69 +16,65 @@
 
 const frankie = {
   name: 'frankie',
-  age: 0,
+  age: 0, // will be set soon
   interests: ['cuddling', 'chasing string', 'catnip'],
-  isGoodWithKids: true,
-  isGoodWithDogs: false,
-  isGoodWithOtherCats: true,
-  getAge: function () {
-    console.log(this.name);
+  generateAge: function () {
     this.age = randomAge(3, 12) + ' months';
-    console.log(this.age);
   }
 };
-frankie.getAge();
-
-
+frankie.generateAge();
 console.log(frankie);
 
-function randomAge (min, max) {
+const jumper = {
+  name: 'jumper',
+  age: 0, // will be set soon
+  interests: ['stalking', 'pouncing', 'toying with prey'],
+  generateAge: function () {
+    this.age = randomAge(3, 18) + ' months';
+  }
+};
+jumper.generateAge();
+console.log(jumper);
+
+// "standalone" functions can still be used
+function randomAge(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // we need to:
 // get (from the DOM) who the parent element is going to be. where am I attaching this new element
-const parentElement = document.getElementById('kittenProfiles');
+const kittenContainerElement = document.getElementById('kittenProfiles');
 
-// create a new element, or elements, that represent frankie
-// article
-// inside that article, h2 for the name, paragraph with their age, ul and some lis with their interests, image
-const article = document.createElement('article');
-parentElement.appendChild(article);
+function renderKitty(kitty) {
+  // create a new element, or elements, that represents frankie
+  // article
+  // inside that article, h2 for the name, paragraph with their age, ul and some lis with their interests, image
+  const article = document.createElement('article');
+  kittenContainerElement.appendChild(article);
 
-const h2 = document.createElement('h2');
-h2.textContent = frankie.name;
-article.appendChild(h2);
+  const h2 = document.createElement('h2');
+  h2.textContent = kitty.name;
+  article.appendChild(h2);
 
-const p = document.createElement('p');
-p.textContent = 'Frankie is adorable, and is ' + frankie.age + ' old.';
-article.appendChild(p);
+  const p = document.createElement('p');
+  p.textContent = kitty.name + ' is adorable, and is ' + kitty.age + ' old.';
+  article.appendChild(p);
 
-const ul = document.createElement('ul');
-article.appendChild(ul);
+  const ul = document.createElement('ul');
+  article.appendChild(ul);
 
-for (let i = 0; i < frankie.interests.length; i++) {
-  const li = document.createElement('li');
-  li.textContent = frankie.interests[i];
-  ul.appendChild(li);
+  for (let i = 0; i < kitty.interests.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = kitty.interests[i];
+    ul.appendChild(li);
+  }
+
+  const img = document.createElement('img');
+  img.setAttribute('src', 'images/' + kitty.name + '.jpeg');
+  img.setAttribute('alt', 'cute picture of ' + kitty.name);
+  article.appendChild(img);
 }
 
-const img = document.createElement('img');
-img.setAttribute('src', 'images/' + frankie.name + '.jpeg');
-img.setAttribute('alt', 'cute picture of ' + frankie.name + ', who is an orange and white cat. You should really adopt him.');
-article.appendChild(img);
+renderKitty(frankie);
+renderKitty(jumper);
 
-// attach those elements to the parent in the DOM.
-
-// object literals
-// bracket and dot notation
-// math.random
-// add kittens to the page
-// each kitten gets an Article
-// each kitten's name shown as a subhead
-// each pic shown as an image with dynamically-created filename (involves setting attributes)
-// each array of interests shown as a list
-// getElementById
-// createElement
-// createTextNode
-// appendChild
