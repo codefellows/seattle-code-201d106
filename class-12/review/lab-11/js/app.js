bc790291942961aa0e0434197c98764eeee544da
JB Tellez <jb.tellez@gmail.com>
2023-11-07 10:57:08 -0800

    updates review code

--- class-12/review/lab-11/js/app.js
+++ class-12/review/lab-11/js/app.js
@@ -1,60 +1,106 @@
 'use strict';
 
+//////////////////////
+// globals
+//////////////////////
+
+const productNames = ['boots', 'bathroom', 'breakfast', 'bubblegum', 'chair', 'dog-duck', 'tauntaun', 'scissors', 'water-can', 'wine-glass', 'bag', 'banana', 'cthulhu', 'dragon', 'pen', 'pet-sweep', 'shark', 'sweep', 'unicorn'];
 const leftImg = document.getElementById('img1');
 const middleImg = document.getElementById('img2');
 const rightImg = document.getElementById('img3');
-const viewResultsButton = document.getElementById('showResults');
-const resultsSection = document.getElementById('results');
-const productNames = ['boots', 'bathroom', 'breakfast', 'bubblegum', 'chair', 'dog-duck', 'tauntaun', 'scissors', 'water-can', 'wine-glass', 'bag', 'banana', 'cthulhu', 'dragon', 'pen', 'pet-sweep', 'shark', 'sweep', 'unicorn'];
-const allProducts = [];
-const maxRounds = 25;
-let workingProducts = [];
+const showResultsButton = document.getElementById('showResults');
+const showResultsSection = document.getElementById('results');
 let leftProduct = null;
 let middleProduct = null;
 let rightProduct = null;
-let roundCtr = 0;
+const maxRounds = 25;
+let currentRound = 0;
 
 
+//////////////////////
+// functions
+//////////////////////
 function Product(name, src) {
   this.name = name;
   this.src = src;
   this.votes = 0;
   this.views = 0;
 }
+Product.allProducts = [];
+Product.workingProducts = [];
+
 
 function initProducts() {
-  for (let productName of productNames) {
+  // for(let item of array) {} OPTIONAL alternate style
+  // for(let productName of productNames) {
+  //   const productInstance = new Product(productName, `imgs/${productName}.jpg`);
+  //   Product.allProducts.push(productInstance);
+  // }
+
+  // the classic way is this
+  for (let i = 0; i < productNames.length; i++) {
+    const productName = productNames[i];
     const productInstance = new Product(productName, `imgs/${productName}.jpg`);
-    allProducts.push(productInstance);
+    Product.allProducts.push(productInstance);
   }
 }
 
 function renderProducts() {
 
-  if(roundCtr == maxRounds) {
+  if (currentRound === maxRounds) {
     endVoting();
-  } else {
-    roundCtr += 1;
+    return;
   }
 
-  if (workingProducts.length < 3) {
-    workingProducts = allProducts.slice();
-    shuffleArray(workingProducts);
+  currentRound += 1;
+
+  if (Product.workingProducts.length < 3) {
+    Product.workingProducts = Product.allProducts.slice();
+    shuffleArray(Product.workingProducts);
   }
 
-  leftProduct = workingProducts.pop();
-  middleProduct = workingProducts.pop();
-  rightProduct = workingProducts.pop();
-
-  leftImg.setAttribute('src', leftProduct.src);
-  middleImg.setAttribute('src', middleProduct.src);
-  rightImg.setAttribute('src', rightProduct.src);
+  leftProduct = Product.workingProducts.pop();
+  middleProduct = Product.workingProducts.pop();
+  rightProduct = Product.workingProducts.pop();
 
   leftProduct.views += 1;
   middleProduct.views += 1;
   rightProduct.views += 1;
+
+  leftImg.setAttribute('src', leftProduct.src);
+  middleImg.setAttribute('src', middleProduct.src);
+  rightImg.setAttribute('src', rightProduct.src);
 }
 
+function endVoting() {
+  leftImg.removeEventListener('click', handleLeftProductClick);
+  middleImg.removeEventListener('click', handleMiddleProductClick);
+  rightImg.removeEventListener('click', handleRightProductClick);
+
+  showResultsButton.hidden = false;
+  showResultsButton.addEventListener('click', handleShowResultsClick);
+}
+
+function handleShowResultsClick() {
+  renderResults();
+}
+
+function renderResults() {
+
+  const ul = document.createElement('ul');
+  showResultsSection.appendChild(ul);
+
+  // for(let productInstance of Product.allProducts) { // optional new way
+  for (let i = 0; i < Product.allProducts.length; i++) { // classic way
+    const productInstance = Product.allProducts[i]; // classic
+    const result = `The product ${productInstance.name} received ${productInstance.votes} votes and was viewed ${productInstance.views} times.`;
+    const li = document.createElement('li');
+    ul.appendChild(li);
+    li.textContent = result;
+  }
+}
+
+// Fisher Yates via Chat GPT
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // Generate a random index from 0 to i
@@ -62,50 +108,42 @@ function shuffleArray(array) {
   }
 }
 
-function handleLeftImgClick() {
+function initEventListeners() {
+  leftImg.addEventListener('click', handleLeftProductClick);
+  middleImg.addEventListener('click', handleMiddleProductClick);
+  rightImg.addEventListener('click', handleRightProductClick);
+}
+
+function handleLeftProductClick() {
+  // tally the votes and swap images / go to next round
   leftProduct.votes += 1;
   renderProducts();
 }
-function handleMiddleImgClick() {
+
+function handleMiddleProductClick() {
+  // tally the votes and swap images / go to next round
   middleProduct.votes += 1;
   renderProducts();
 }
-function handleRightImgClick() {
+
+function handleRightProductClick() {
+  // tally the votes and swap images / go to next round
   rightProduct.votes += 1;
   renderProducts();
 }
 
-function endVoting() {
-  viewResultsButton.hidden = false;
-  leftImg.removeEventListener('click', handleLeftImgClick);
-  middleImg.removeEventListener('click', handleMiddleImgClick);
-  rightImg.removeEventListener('click', handleRighImgClick);
-}
-
-function handleViewResultsClick() {
-  resultsSection.innerHTML = '';
-  const ul = document.createElement('ul');
-  resultsSection.appendChild(ul);
-  for(let product of allProducts) {
-    const li = document.createElement('li');
-    ul.appendChild(li);
-    const result = `${product.name} was viewed ${product.views} times and voted for ${product.votes} times.`;
-    li.textContent = result;
-  }
-}
-
-function initEventListeners() {
-  leftImg.addEventListener('click', handleLeftImgClick);
-  middleImg.addEventListener('click', handleMiddleImgClick);
-  rightImg.addEventListener('click', handleRightImgClick);
-  viewResultsButton.addEventListener('click', handleViewResultsClick);
-}
-
-function start() {
+function startApp() {
   initProducts();
   initEventListeners();
   renderProducts();
 }
 
-start();
+///////////////////////
+// Start App
+///////////////////////
+startApp();
+
+
+
+
 
